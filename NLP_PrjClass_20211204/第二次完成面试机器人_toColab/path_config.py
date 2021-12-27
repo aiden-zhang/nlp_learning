6756d2dfa09e0086f8c9b379cdf035e776ac9c2d
aiden-zhang <wyzhangning@163.com>
2021-12-27 23:48:22 +0800

    增加第二次完成对话机器人及pytorch学习的文件

---
+++ NLP_PrjClass_20211204/第二次完成面试机器人_toColab/path_config.py
@@ -0,0 +1,10 @@
+#GRADE_MODEL_PATH = 'src/grade/'
+#GRADE_MODEL_PATH = 'grade/'  #在src下执行manage.py时使用
+GRADE_MODEL_PATH = '../src/grade/'  #在InterviewRobot下执行grade_model.py时使用
+
+#KEYWORD_MODEL_PATH = 'src/keyword/'
+#KEYWORD_MODEL_PATH = 'keyword/'    在InterviewRobot下执行keyword_model.py时使用
+KEYWORD_MODEL_PATH = '../src/keyword/'  #在InterviewRobot下执行manage.py时使用
+NEO4JIP = 'http://localhost:7474'
+NEO4JUSER = 'secondClass'
+NEO4JPASSWORD = '123456'
